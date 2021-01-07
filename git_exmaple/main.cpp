ddb955792c4d7eb38e0266cefc0a13fec1388ddc
Jorge <eduardojorge116@gmail.com>
2021-01-07 00:33:15 -0600

    Initial Commit

---
+++ git_exmaple/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  git_exmaple
+//
+//  Created by Administrador on 07/01/21.
+//  Copyright © 2021 Administrador. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
