625a7cfd5f87036422389ca523acc9ac42c8ad42
Jorge <eduardojorge116@gmail.com>
2021-01-07 01:44:27 -0600

    se agrego código para preguntar y verificar seguridad de contraseña

--- git_exmaple/main.cpp
+++ git_exmaple/main.cpp
@@ -7,9 +7,56 @@
 //
 
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main() {
+    
+    bool error = 0;
+    string password;
+    char  condicion = '@';
+  
+    
+   
+    
+    while (true) {
+        
+        cout<<"escribe tu contraseña: ";
+        cin>>password;
+        
+        int n = password.length();
+        
+        char contra[n+1];
+        
+        strcpy(contra, password.c_str());
+        cout<<contra;
+        for (int j = 0; j <= n+1; j++) {
+            
+            cout<<contra[j]<<endl;
+            
+        }
+        
+        for (int j = 0; j <= n+1; j++) {
+            
+            if (contra[j] == condicion ) {
+                error = 0;
+                break;
+            } else {
+                error = 1;
+               
+            }
+    
+        }
+        
+        if (error == 0) {
+            break;
+        } else{
+            cout<<"le falta un 'J' a tu contraseña"<<endl;
+        }
+        
+    }
+    
+    cout<<"tu contraseña es: "<<password<<endl;
+    
+    
     return 0;
 }
